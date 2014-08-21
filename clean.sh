a0cf808c39d08470850b21ceca689cfb0ccad1c0
fabien sanglard <fabien.sanglard@gmail.com>
2014-08-21 00:26:27 -0700

    Add .gitignore for latex.

--- clean.sh
+++ clean.sh
@@ -1,2 +1,7 @@
 find . -name "*.pdf" -exec rm {} \;
 find . -name "*.eps" -exec rm {} \;
+find . -name "*.aux" -exec rm {} \;
+find . -name "*.log" -exec rm {} \;
+find . -name "*.lot" -exec rm {} \;
+find . -name "*.out" -exec rm {} \;
+find . -name "*.toc" -exec rm {} \;
