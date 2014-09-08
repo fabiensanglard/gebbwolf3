a8ba21f9624c632f191b9bcfbecb0b60ed504b0f
fabien sanglard <fabien.sanglard@gmail.com>
2014-09-07 21:42:03 -0700

    Cover wip.

---
+++ src/pointer_madness.c
@@ -0,0 +1,10 @@
+int main(int argc, char** argv){
+
+  far void* a = FP_SEG(0x0000) + FP_OFF(0x0120);
+  far void* b = FP_SEG(0x0010) + FP_OFF(0x0020);
+  far void* c = FP_SEG(0x0012) + FP_OFF(0x0000);
+
+  printf("%b",a==b);
+  printf("%b",a==c);
+  printf("%b",b==c);
+}
