e2ff0770effe0a4d3ddab6a38248ea294609e2ff
MacPro: Fabien Sanglard <fabiensanglard.net@gmail.com>
2016-04-10 10:37:44 -0700

    Added early characters drawings

---
+++ src/code/CalcHeight.c
@@ -0,0 +1,23 @@
+int	CalcHeight (void)
+{
+	fixed gxt,gyt,nx,ny;
+	long	gx,gy;
+
+	gx = xintercept-viewx;
+	gxt = FixedByFrac(gx,viewcos);
+
+	gy = yintercept-viewy;
+	gyt = FixedByFrac(gy,viewsin);
+
+	nx = gxt-gyt;
+
+  //
+  // calculate perspective ratio (heightnumerator/(nx>>8))
+  //
+	if (nx<mindist)
+		nx=mindist;			// don't let divide overflow
+
+	asm	mov	ax,[WORD PTR heightnumerator]
+	asm	mov	dx,[WORD PTR heightnumerator+2]
+	asm	idiv	[WORD PTR nx+1]			// nx>>8
+}
