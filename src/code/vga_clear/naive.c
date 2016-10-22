9fdfa07b191d518aec4d287a23949894e1e0f4ea
MacPro: Fabien Sanglard <fabiensanglard.net@gmail.com>
2016-10-21 23:51:11 -0700

    Add sprites results of drawing in asset workflow

--- src/code/vga_clear/naive.c
+++ src/code/vga_clear/naive.c
@@ -2,12 +2,12 @@ byte far *VGA = (byte far*)0xA0000000L;
 char color = 0;
 
 for (int x = 0 ; x < 320 ; x++) {
-	for (int y=0 ; y < 200 ; y++) {
-	  /* select plane */
-       outp(SC_INDEX, MAP_MASK);
-       outp(SC_DATA,  1 << (x&3) );
+    for (int y=0 ; y < 200 ; y++) {
+	    /* select plane */
+        outp(SC_INDEX, MAP_MASK);
+        outp(SC_DATA,  1 << (x&3) );
 
-       VGA[(y<<6)+(y<<4)+(x>>2)]=color;
+        VGA[(y<<6)+(y<<4)+(x>>2)]=color;
 	}
 }
 
--- src/code/vga_clear/optimized.c
+++ src/code/vga_clear/optimized.c
@@ -6,7 +6,7 @@ for (int x = 0 ; x < 320 ; x++) {
     outp(SC_INDEX, MAP_MASK);
     outp(SC_DATA,  1 << (x&3) );
     for (int y=0 ; y < 200 ; y++) {
-       VGA[(y<<6)+(y<<4)+(x>>2)]=color;
+        VGA[(y<<6)+(y<<4)+(x>>2)]=color;
 	}
 }
 
