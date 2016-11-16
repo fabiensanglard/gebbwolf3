da0440f148275811445db641cc53512a80aa1d89
MacPro: Fabien Sanglard <fabiensanglard.net@gmail.com>
2016-11-15 23:08:37 -0800

    An other 6 hours of work tonight.

---
+++ src/code/assets_header.c
@@ -0,0 +1,23 @@
+//////////////////////////////////////
+//
+// Graphics .H file for .WL1
+// IGRAB-ed on Sun May 03 01:19:32 1992
+//
+//////////////////////////////////////
+
+typedef enum {
+		// Lump Start
+		H_BJPIC=3,
+		H_CASTLEPIC,                 // 4
+		H_KEYBOARDPIC,               // 5
+		H_JOYPIC,                    // 6
+		H_HEALPIC,                   // 7
+		H_TREASUREPIC,               // 8
+		H_GUNPIC,                    // 9
+		H_KEYPIC,                    // 10
+		H_BLAZEPIC,                  // 11
+		H_WEAPON1234PIC,             // 12
+		H_WOLFLOGOPIC,               // 13
+		...
+		PAUSEDPIC,                   // 140
+		GETPSYCHEDPIC,               // 141
---
+++ src/code/assets_usage.c
@@ -0,0 +1,14 @@
+void CheckKeys (void) {}
+  if (Paused) {
+    ...
+    LatchDrawPic (20-4,80-2*8,PAUSEDPIC);
+    ...
+  }
+}    
+
+
+void PreloadGraphics(void) {
+	...
+	LatchDrawPic (20-14,80-3*8,GETPSYCHEDPIC);
+	...
+}
