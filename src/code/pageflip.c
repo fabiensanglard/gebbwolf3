85e368e4a9d16426d5abfcaa3fb57acdde8c98af
MacPro: Fabien Sanglard <fabiensanglard.net@gmail.com>
2016-12-26 21:44:58 -0800

    almost done on vga solving part

---
+++ src/code/pageflip.c
@@ -0,0 +1,15 @@
+  asm mov cx, startScanOffset 
+
+  asm mov dx,3d4h  // 3d4h is the CRTC register
+
+  asm mov al,0ch  // Tell the CRTC we want to update 
+  asm out dx,al       // the start address high register
+  asm inc dx
+  asm mov al,ch
+  asm out dx,al    // set the high byte
+
+  asm mov al,0dh  // Tell the CRTC we want to update 
+  asm out dx,al       // start address low register
+  asm inc dx
+  asm mov al,cl
+  asm out dx,al    // set the low byte
