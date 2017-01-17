0a050b9392b2a08533e92661c1e0fb98b60675c4
fabien sanglard <fabien.sanglard@gmail.com>
2017-01-16 23:47:54 -0800

    Improved compscale summary

--- src/code/compscale.c
+++ src/code/compscale.c
@@ -1,19 +1,27 @@
+// Call with
+// DS:SI   Source for scale
+// ES:DI   Dest for scale
 unsigned BuildCompScale (int height, byte far *code)
-  ...
+  [...]
   work = (t_compscale far *)code;
   code = &work->code[0];
+  [...]
+  for (src=0;src<=64;src++) {
 
-  // mov al,[si+src]
-  *code++ = 0x8a;
-  *code++ = 0x44;
-  *code++ = src;  
+    [...]
+    
+    // mov al,[si+src]   (Read src into register al)
+    *code++ = 0x8a;
+    *code++ = 0x44;
+    *code++ = src;  
 
-  for (;startpix<endpix;startpix++) {
-    // mov [es:di+heightofs],al
-    *code++ = 0x26;
-    *code++ = 0x88;
-    *code++ = 0x85;
-    *((unsigned far *)code)++ = startpix*SCREENBWIDE;
+    for (;startpix<endpix;startpix++) {
+      // mov [es:di+heightofs],al  (Write al to dest)
+      *code++ = 0x26;
+      *code++ = 0x88;
+      *code++ = 0x85;
+      *((unsigned far *)code)++ = startpix*SCREENBWIDE;
+    }
   }
   // retf
   *code++ = 0xcb;
