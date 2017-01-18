dd206d355ac0f7d62e62a75479c5e7f28eb79dc9
fabien sanglard <fabien.sanglard@gmail.com>
2017-01-17 21:25:01 -0800

    working on 3d

--- src/code/generic_draw.c
+++ src/code/generic_draw.c
@@ -1,6 +1,6 @@
 void scaleTextureToHeight(int height, void* src, void* dst){
   fixed_t steps = ...;
-  for (int cursor=0;i<steps;steps++) {
+  for (int cursor=0 ; cursor<steps ; cursor++) {
   	int texture_offset = ...;
   	int dst_offset = ...;
   	dst[dst_offset] = src[texture_offset];
