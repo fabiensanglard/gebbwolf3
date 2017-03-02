3d660bdd1d8c9137ae24e3a4249881094661d21d
Aurelien Sanglard <aurelien.sanglard@orange.com>
2017-03-02 22:25:16 +0100

    [Make_Improvement] Create output and build directories if they do not exist

--- make.sh
+++ make.sh
@@ -1,6 +1,13 @@
-mkdir output
+if [ ! -d output ]
+then
+   mkdir output
+fi
 cd src
 find . -name "*.eps" -exec epstopdf '{}' \;
 pdflatex -output-directory ../output book.tex
 cd ..
+if [ ! -d build ]
+then
+   mkdir build
+fi
 cp output/book.pdf build/book.pdf
