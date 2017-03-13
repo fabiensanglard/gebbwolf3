af9a3803348994862b3eefd5a2c1f038ac55c161
fabien sanglard <fabien.sanglard@gmail.com>
2017-03-13 00:57:24 -0700

    Lot of re-writing

---
+++ src/code/num_areas.c
@@ -0,0 +1,4 @@
+#define NUMAREAS		37
+
+byte		far areaconnect[NUMAREAS][NUMAREAS];
+boolean		areabyplayer[NUMAREAS];
--- src/code/unrolled_loop_playloop.c
+++ src/code/unrolled_loop_playloop.c
@@ -1,16 +1,12 @@
 void PlayLoop () {
   PollControls ();         // Get player input
+
   MoveDoors ();            // Move doors
   MovePWalls ();           // Move secret wall
-  
   for (obj = player; obj; obj = obj->next)
     DoActor (obj);         // Enemies think
 
-  UpdatePaletteShifts ();  // Damage or Bonus
-
   ThreeDRefresh () {       // Render 3D view
-    // clear out the traced array
-    CalcViewVariables();    
     VGAClearScreen ();      // Draw floor/ceiling
     WallRefresh ();         // Draw walls
     DrawScaleds();          // draw scaled stuff
