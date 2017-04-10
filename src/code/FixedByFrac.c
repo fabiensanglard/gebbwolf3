dc2cc4c83b707a45ba1b843db71ff60cdf3da8eb
fabien sanglard <fabien.sanglard@gmail.com>
2017-04-10 02:23:35 -0700

    Proof reading. Landed in SFO

---
+++ src/code/FixedByFrac.c
@@ -0,0 +1,32 @@
+fixed FixedByFrac (fixed a, fixed b) {
+asm	mov	si,[WORD PTR b+2]	// sign of result = 
+                            // sign of fraction
+asm	mov	ax,[WORD PTR a]
+asm	mov	cx,[WORD PTR a+2]
+
+asm	or	cx,cx
+asm	jns	aok:				// negative?
+asm	neg	cx
+asm	neg	ax
+asm	sbb	cx,0
+asm	xor	si,0x8000			// toggle sign of result
+aok:
+
+// multiply  cx:ax by bx
+asm	mov	bx,[WORD PTR b]
+asm	mul	bx					// fraction*fraction
+asm	mov	di,dx				// di is low word of result
+asm	mov	ax,cx				//
+asm	mul	bx					// units*fraction
+asm add	ax,di
+asm	adc	dx,0
+
+// put result dx:ax in 2's complement
+asm	test	si,0x8000		// is the result negative?
+asm	jz	ansok:
+asm	neg	dx
+asm	neg	ax
+asm	sbb	dx,0
+
+ansok:; // I stick the return value in with ASMs
+}
--- src/code/fixed_mul.c
+++ src/code/fixed_mul.c
@@ -1,5 +1,3 @@
-typedef long fixed;
-
 fixed FixedMul (fixed a, fixed b)
 {
 	return (a>>8)*(b>>8);
