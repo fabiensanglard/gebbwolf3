f3b4dcbb5d425c4620fca15d6a2b565253960fcf
fabien sanglard <fabien.sanglard@gmail.com>
2017-06-08 18:29:50 -0700

    Aurelien proof reading

--- src/code/soundsystem_interrupt.c
+++ src/code/soundsystem_interrupt.c
@@ -4,15 +4,15 @@ static void SDL_SetTimerSpeed(void) {
 	word	rate;
 	void interrupt	(*isr)(void);
 
-	if (A) {        // 7000 Hz
+	if (PCM_sound) {               // 7000 Hz
 		rate = TickBase * 100;
 		isr = SDL_t0ExtremeAsmService;
 	}
-	else if (B) {   // 700 Hz
+	else if (FM_sound_effects) {   // 700 Hz
 		rate = TickBase * 10;
 		isr = SDL_t0FastAsmService;
 	}
-	else {          // 140 Hz
+	else {   // beeper             // 140 Hz
 		rate = TickBase * 2;
 		isr = SDL_t0SlowAsmService;
 	}
