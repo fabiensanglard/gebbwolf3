5f686c6d8ea2471bf26ade05cbd44c68c67477b5
fabien sanglard <fabien.sanglard@gmail.com>
2017-06-15 23:17:18 -0700

    Fixed aspect ratio and source code issues.

--- src/code/pass_0_1.c
+++ src/code/pass_0_1.c
@@ -1,2 +1,2 @@
-PASS1: mapmasks1[0][2] = 3
-PASS2: mapmasks2[0][2] = 0 -> EXIT
+PASS1: mapmasks1[0][1] = 3
+PASS2: mapmasks2[0][1] = 0 -> EXIT
--- src/code/pass_3_4.c
+++ src/code/pass_3_4.c
@@ -1,3 +1,3 @@
-PASS1: mapmasks1[3][2] = 8
-PASS2: mapmasks2[3][2] = 1
-PASS3: mapmasks3[3][2] = 0 -> EXIT
+PASS1: mapmasks1[3][1] = 8
+PASS2: mapmasks2[3][1] = 1
+PASS3: mapmasks3[3][1] = 0 -> EXIT
