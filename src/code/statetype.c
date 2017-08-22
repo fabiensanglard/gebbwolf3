8c8c3dd876e60e2afd4bfa70b08fc1c1967aff1b
fabien sanglard <fabiensanglard@fabiens-mac-pro.lan>
2017-08-21 23:41:26 -0700

    More proofreading fixes

--- src/code/statetype.c
+++ src/code/statetype.c
@@ -1,8 +1,8 @@
-typedef struct	statestruct
+typedef struct    statestruct
 {
-	boolean	rotate;
-	int		shapenum;	// Sprite to render on screen
-	int		tictime;    // How long stay in that state
-	void	(*think) (),(*action) ();
-	struct	statestruct	*next;
+    boolean    rotate;
+    int        shapenum;   // Sprite to render on screen
+    int        tictime;    // How long stay in that state
+    void    (*think) (),(*action) ();
+    struct    statestruct    *next;
 } statetype;
