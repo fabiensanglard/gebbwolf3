2a00fcbfde1f2c8f3b786af6bf5597e52c60263b
fabien sanglard <fabien.sanglard@gmail.com>
2017-08-22 01:06:44 -0700

    Fix layout

--- src/code/build_vis_list.c
+++ src/code/build_vis_list.c
@@ -1,29 +1,29 @@
-#define MAXVISABLE    50
+#define MAXVISABLE  50
 
 typedef struct {
-    int    viewx,viewheight,shapenum;
+  int  viewx,viewheight,shapenum;
 } visobj_t;
 
-visobj_t    vislist[MAXVISABLE],*visptr,*visstep,*farthest;
+visobj_t  vislist[MAXVISABLE],*visptr,*visstep,*farthest;
 
 void DrawScaleds (void) {
-    int numvisable=0;
-    visptr = &vislist[0];
+  int numvisable=0;
+  visptr = &vislist[0];
 
-    // Use spotvis[] to add objects to visptr. Increase numvisable
+  // Use spotvis[] to add objects to visptr. Increase numvisable
 
-    // draw from back to front
-    for (i = 0; i<numvisable; i++){
-        least = 32000;
-        for (visstep=&vislist[0] ; visstep<visptr ; visstep++){
-            height = visstep->viewheight;
-            if (height < least){
-                least = height;
-                farthest = visstep;
-            }
-        }
-        ScaleShape(farthest->x,farthest->id,farthest->height); 
-        farthest->viewheight = 32000;
+  // draw from back to front
+  for (i = 0; i<numvisable; i++){
+    least = 32000;
+    for (visstep=&vislist[0] ; visstep<visptr ; visstep++){
+      height = visstep->viewheight;
+      if (height < least){
+        least = height;
+        farthest = visstep;
+      }
     }
+    ScaleShape(farthest->x,farthest->id,farthest->height); 
+    farthest->viewheight = 32000;
+  }
 
 }
