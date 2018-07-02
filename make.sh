1dd2ad33c040fecf11e57961f9587c7031720f4c
Fabien Sanglard <sanglardf@google.com>
2018-07-01 18:34:34 -0700

    First commit

--- make.sh
+++ make.sh
@@ -3,7 +3,29 @@ then
    mkdir output
 fi
 cd src
-find . -name "*.eps" -exec epstopdf '{}' \;
+
+epsToPDF () {	
+    # Build path without extension.
+	filename=$(basename "$1")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+	dir=$(dirname $1)
+
+	extensionless=$dir/$filename
+
+  # -nt = newer than. -ot = older than.
+	if [ "$extensionless".eps -nt "$extensionless".pdf ]; then
+		echo "Convert $extensionless.eps to PDF."
+    epstopdf "$extensionless".eps
+  fi
+  # epstopdf $filename
+}
+
+# Convert eps to pdf if necessary
+find . -name "*.eps" | while read file; do epsToPDF "$file"; done
+# find . -name "*.eps" -exec epstopdf '{}' \;
+
+
 pdflatex -output-directory ../output book.tex
 cd ..
 if [ ! -d build ]
