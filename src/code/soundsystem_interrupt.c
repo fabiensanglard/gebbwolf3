1e58c5e1de403a44470b2260bc2ceb1692d66e47
Fabien Sanglard <sanglardf@google.com>
2018-07-01 22:39:24 -0700

    Redone SDL_SetTimerSpeed

--- src/code/soundsystem_interrupt.c
+++ src/code/soundsystem_interrupt.c
@@ -1,14 +1,23 @@
 #define	TickBase	70
 
+typedef	enum {
+  sds_Off,
+  sds_PC,
+  sds_SoundSource,
+  sds_SoundBlaster
+} SDSMode;
+extern	SDSMode		DigiMode;
+
 static void SDL_SetTimerSpeed(void) {
 	word	rate;
 	void interrupt	(*isr)(void);
 
-	if (DigiMode == PCSpeaker) && DigiPlaying) { 
+	if (DigiMode == sds_PC) && DigiPlaying) { 
 		rate = TickBase * 100;     // 7000 Hz
 		isr = SDL_t0ExtremeAsmService;
 	}
-	else if (music || ((DigiMode == Dss) && DigiPlaying) {   
+	else if (music || ((DigiMode == sds_SoundSource) 
+		&& DigiPlaying) {   
 		rate = TickBase * 10;     //  700 Hz
 		isr = SDL_t0FastAsmService;
 	}
@@ -16,7 +25,6 @@ static void SDL_SetTimerSpeed(void) {
 		rate = TickBase * 2;      //  140 Hz
 		isr = SDL_t0SlowAsmService;
 	}
-
 	setvect(8,isr);
 	SDL_SetIntsPerSec(rate);
 }
