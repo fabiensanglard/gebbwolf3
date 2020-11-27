fc164fc9f02b6b8dab872e86c1f46f95145d4fa4
Fabien Sanglard <fabien.sanglard@gmail.com>
2020-11-27 15:58:29 -0800

    Fix interrupt id (0x10 = 16, not 19)

--- src/code/init_vga.c
+++ src/code/init_vga.c
@@ -1,5 +1,5 @@
 void	VL_SetVGAPlaneMode (void) {
-  // Call 19th interrupt vector with value 0x13
+  // Call 16th interrupt vector with value 0x13
   // (Ask the BIOS to setup the VGA in mode 13h)
   asm	mov	ax,0x13
   asm	int	0x10
