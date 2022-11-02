09fa61003ec6efc210e09a18fcf4774baeb10c0b
Fabien Sanglard <fabien.sanglard@gmail.com>
2022-11-02 13:12:10 -0700

    Fix layout in debug build

--- src/code/clear_vis_wold3d.c
+++ src/code/clear_vis_wold3d.c
@@ -2,5 +2,5 @@ asm mov	ax,ds
 asm mov es,ax
 asm mov di,OFFSET spotvis // Array to clear
 asm xor ax,ax   // Put 0 in ax
-asm movc x,2048	// repeat next instruction 64*64/2
+asm mov cx,2048	// repeat next instruction 64*64/2
 asm rep stosw   // store ax at es:di 
